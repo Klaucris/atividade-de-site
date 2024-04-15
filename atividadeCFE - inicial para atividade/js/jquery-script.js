fc5204f78b96e942ca4a8cf1203547b2c31e1399
Klaucris <159507043+Klaucris@users.noreply.github.com>
2024-04-15 05:30:13 -0300

    Initial commit

---
+++ atividadeCFE - inicial para atividade/js/jquery-script.js
@@ -0,0 +1,10 @@
+//efeito de esconder formulário de cadastro
+$(document).ready(function(){
+    $("botão-cadastrar").click(function(){
+    $("#form-cadastrar").slideToggle("slow");
+    $("#section-login").slideToggle("slow");
+
+    $("#botão-cadastrar").hide();
+
+    });
+    });
